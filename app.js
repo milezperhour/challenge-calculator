5566085274ae0b514d88d42dfe9c3883cb7e4f06
milezperhour <hyperbolvoid@gmail.com>
2019-09-26 12:22:35 -0700

    setup constructor

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ class App extends React.Component {
     }
 }
 
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {number: ''};
+    this.handleNumberInput = this.handleNumberInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('app')
